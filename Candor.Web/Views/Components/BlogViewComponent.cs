de111b601d199c93f9118bdbc2c8c9298d79ede2
Alexander Makarov <Joker124rus@gmail.com>
2022-08-22 17:32:14 +0700

    Comments

--- Candor.Web/Views/Components/BlogViewComponent.cs
+++ Candor.Web/Views/Components/BlogViewComponent.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Candor.Web.Views.Components;
 
+/// <summary>
+/// Blog view component.
+/// </summary>
 public class BlogViewComponent : ViewComponent
 {
+    /// <summary>
+    /// Invokes component.
+    /// </summary>
+    /// <param name="posts">Blog posts.</param>
+    /// <returns>View with posts.</returns>
     public IViewComponentResult Invoke(IEnumerable<Post> posts)
     {
         return View("Blog", posts);
